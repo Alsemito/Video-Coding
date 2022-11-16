246b51e272382d355f9d9a8ca737b39682327f24
Alex Serrano Marull <alex.serrano01@estudiant.upf.edu>
2022-11-16 17:56:11 +0100

    No comments

--- S3/S3_AlexSerrano_230278.py
+++ S3/S3_AlexSerrano_230278.py
@@ -15,11 +15,19 @@ def video_scaler(inPath: str):
 
 def video_converter(inPath: str):
 
-    os.system("ffmpeg -i " + inPath + " -c:v libvpx -b:v 1M -c:a libvorbis vp8.webm")  # VP8
-    os.system("ffmpeg -i " + inPath + " -c:v libvpx-vp9 -b:v 1M -c:a libvorbis vp9.webm")  # VP9
-    os.system("ffmpeg -i " + inPath + " -c:v libx265 -crf 26 -preset fast -c:a ac3 -b:a 256k h265.mp4")  # H.265
-    os.system("ffmpeg -i " + inPath + " -c:v libaom-av1 -crf 26 -b:v 1M -c:a ac3 -b:a 256k av1.mkv")  # AV1
+    os.system("ffmpeg -i " + inPath + " -c:v libvpx -c:a libvorbis vp8.webm")  # VP8
+    os.system("ffmpeg -i " + inPath + " -c:v libvpx-vp9 -c:a libvorbis vp9.webm")  # VP9
+    os.system("ffmpeg -i " + inPath + " -c:v libx265 -c:a ac3 -b:a 256k h265.mp4")  # H.265
+    os.system("ffmpeg -i " + inPath + " -c:v libaom-av1 -c:a ac3 -b:a 256k av1.mkv")  # AV1
 
+
+def create4video():
+    #ffmpeg -i input.m2ts -filter_complex "[0]crop=iw/2:ih:0:0[left];[0]crop=iw/2:ih:ow:0[right]"
+    # -map "[left]" -map "[right]" -map 0:a output.mp4
+
+    os.system("ffmpeg -i vp8.webm -i vp9.webm -filter_complex hstack output.mp4")
+    os.system("ffmpeg -i h265.mp4 -i av1.mkv -filter_complex hstack output2.mp4")
+    os.system("ffmpeg -i output.mp4 -i output2.mp4 -filter_complex vstack 4stack.mp4")
 
 loop = 1
 if __name__ == '__main__':
@@ -31,6 +39,7 @@ if __name__ == '__main__':
                 option = int(input('\n0 - Navigate by typing the number of the exercise:\n'
                                    '1 - Resize the input video to 720p, 480p, 360x240 and 160x120\n'
                                    '2 - Convert to different video codecs\n'
+                                   '3 - Create the video with 4 videos\n'
                                    ''))
             case 1:
                 user_in1 = str(input("Enter the name of the video (with its extension, e.g. .mp4): "))
@@ -40,5 +49,9 @@ if __name__ == '__main__':
             case 2:
                 user_in2 = str(input("Enter the name of the video (with its extension, e.g. .mp4): "))
                 video_converter(user_in2)
+                option = 0
+
+            case 3:
+                create4video()
                 option = 0
 
